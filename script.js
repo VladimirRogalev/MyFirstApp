0a35851d85c3c0a51b02fc7d111625341a0a1197
Vladimir <vladimirrogalev1@gmail.com>
2024-03-29 19:31:42 +0300

    second commit

--- script.js
+++ script.js
@@ -8,12 +8,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('One of the last movies you watched?', ''),
-      b = prompt('How much would you rate it?',''),
-      c = prompt('One of the last movies you watched?', ''),
-      d = prompt('How much would you rate it?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for( let i = 0; i < 2; i++) {
+    const a = prompt('One of the last movies you watched?', ''),
+    b = prompt('How much would you rate it?','');
+
+    if (a != null && b != null && a !='' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error')
+        i--;
+    }
+    
+}
+
+if(personalMovieDB.count < 10) {
+    console.log("Watched quite a few films")
+} else if (personalMovieDB.count >= 10 || personalMovieDB.count < 30) {
+console.log('You are a classic viewer!');
+} else if ( personalMovieDB >= 30) {
+    console.log('You are a movie buff!');
+} else {
+    console.log('An error has occurred');
+}
+
 
 console.log(personalMovieDB);
